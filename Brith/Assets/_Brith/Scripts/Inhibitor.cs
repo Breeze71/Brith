856d30326af3b89bc81e69dce969d01d50b5c467
V <j92080412@gmail.com>
2023-11-27 13:11:29 +0800

    FixHealthBarUI

--- Brith/Assets/_Brith/Scripts/Inhibitor.cs
+++ Brith/Assets/_Brith/Scripts/Inhibitor.cs
@@ -10,7 +10,7 @@ namespace V
     {
         [field : SerializeField] public int HealthAmount { get; set; }
         public HealthSystem HealthSystem { get; set; }
-        public HealthBarUI HealthBarUI { get; set; }
+        [field :SerializeField]public HealthBarUI HealthBarUI { get; set; }
 
 
         [SerializeField] private GameObject techTreeUI;
