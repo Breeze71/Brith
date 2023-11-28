8b2183802720718ae81189ceab9205a42ea8569f
demineer <2361368702@qq.com>
2023-11-28 23:07:45 +0800

    ui

--- Brith/Assets/_Brith/Scripts/RandomSpawn/SpawnEntity.cs
+++ Brith/Assets/_Brith/Scripts/RandomSpawn/SpawnEntity.cs
@@ -7,22 +7,37 @@ namespace V
     public class SpawnEntity : MonoBehaviour
     {
         [SerializeField] private Collider2D coll;
-        [SerializeField] private GameObject[] entities;
+        [SerializeField] private GameObject[] FireOrigin;
+        [SerializeField] private GameObject[] WaterOrigin;
+        [SerializeField] private GameObject[] WindOrigin;
+        [SerializeField] private GameObject[] GroundOrigin;
+        private List<GameObject[]> entities;
         [SerializeField] private GameObject[] TargetEntity;
         [SerializeField] private GameObject[] Enemy;
         public RoomInfo room;
         public void CreateEntity()
         {
+            //Debug.Log(FireOrigin[1].name);
+            entities = new List<GameObject[]>();
+            entities.Add(FireOrigin);
+            entities.Add(WaterOrigin);
+            entities.Add(WindOrigin);
+            entities.Add(GroundOrigin);
             room = gameObject.GetComponentInParent<RoomInfo>();
+            //spawn target
             if (room.EndRoom)
-                EntitySpawnManager.Instance.SpawnEntities(coll, TargetEntity);
+            {
+                EntitySpawnManager.Instance.SpawnEntities(coll, TargetEntity, gameObject.transform.parent);
+            }
+            //spawn scene entity
             if (room.RoomNumberFromOrigin != 0)
             {
                 EntitySpawnManager.Instance.SpawnEntities(coll, Enemy, room.Number);
             }
 
             #region Spawn SceneEntity
-            EntitySpawnManager.Instance.SpawnEntities(coll, entities);
+            int randomNumber = Random.Range(0, 2);
+            EntitySpawnManager.Instance.SpawnEntities(coll, entities, gameObject.transform.parent, room.Number);
             Room roomdata = NewRoommanagerOnGame.Instance.GetRoomList()[room.Number];
             roomdata.SceneEntityNumber = room.SceneEntity = roomdata.SceneEntityNumber + 1;
             #endregion
---
+++ Brith/Assets/_Brith/Scripts/StartScene/GameMenu.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMenu : MonoBehaviour
+{
+    [SerializeField] private string TechtreeScene;
+    [SerializeField] private string FirstLv;
+    [SerializeField] private string OptionScene;
+    [SerializeField]private string Startscene;
+    public void OnContinnueGame()
+    {
+        //todo
+        SceneManager.LoadScene(TechtreeScene);
+    }
+    public void OnStartGame()
+    {
+        //todo
+        SceneManager.LoadScene(FirstLv);
+    }
+    public void OnOption()
+    {
+        SceneManager.LoadScene(OptionScene);
+    }
+    public void OnExitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+    }
+    public void OptionbackToStart()
+    {
+        SceneManager.LoadScene(Startscene);
+    }
+}
