60e788ce5f2c4c1ff9f0633bdafece999fcac6ab
demineer <2361368702@qq.com>
2023-11-28 14:15:53 +0800

    Revert "Merge branch 'TechTree-PlayerSkill' into Dev——Kaofish"
    This reverts commit 2715d84a72f860947df45268fded722284674b3e, reversing
    changes made to 094ceddff3f6e055a2b23cbfc4f6e1162a1dd7c5.

--- Brith/Assets/_Brith/Scripts/RandomSpawn/EntitySpawnManager.cs
+++ Brith/Assets/_Brith/Scripts/RandomSpawn/EntitySpawnManager.cs
@@ -69,6 +69,7 @@ namespace V
                 {
                     if(((1 << _coll.gameObject.layer) & layerNotToSpawn.value) != 0)
                     {
+                        Debug.Log("valid");
                         _isInValidCollision = true;
                         break;
                     }
