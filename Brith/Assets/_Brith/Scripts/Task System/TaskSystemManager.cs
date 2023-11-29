6479d193fc925526de12f315e15be598003df232
demineer <2361368702@qq.com>
2023-11-29 11:47:30 +0800

    Update TaskSystemManager.cs

--- Brith/Assets/_Brith/Scripts/Task System/TaskSystemManager.cs
+++ Brith/Assets/_Brith/Scripts/Task System/TaskSystemManager.cs
@@ -7,6 +7,9 @@ namespace V
     public class TaskSystemManager : MonoBehaviour
     {
         [SerializeField] private List<TaskData> tasks;
+        List<TaskData> tasksOnGame;
+        int task1_ID;
+        int task2_ID;
         public static TaskSystemManager Instance { get; set; }
         private void Awake()
         {
@@ -19,12 +22,15 @@ namespace V
         }
         private void Start()
         {
-            int task1_ID = Random.Range(0, 4);
-            int task2_ID = Random.Range(0, 4);
+            task1_ID = Random.Range(0, 4);
+            task2_ID = Random.Range(0, 4);
             while (task1_ID==task2_ID)
             {
                 task2_ID=Random.Range(0, 4);
             }
+            tasksOnGame = new List<TaskData>();
+            tasks.Add(tasks[task1_ID]);
+            tasksOnGame.Add(tasks[task2_ID]);
         }
     }
 }
