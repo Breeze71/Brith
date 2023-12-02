f15734bd4d63d48297699340d876b53697656b04
V <j92080412@gmail.com>
2023-12-02 10:51:39 +0800

    Test Canvas

--- Brith/Assets/_Brith/Scripts/UI/TestUI.cs
+++ Brith/Assets/_Brith/Scripts/UI/TestUI.cs
@@ -7,17 +7,12 @@ namespace V
 {
     public class TestUI : MonoBehaviour
     {
-        [SerializeField] private Button lossButton;
         [SerializeField] private Button winButton;
         private CellTech cellTech;
         private void Awake() 
         {
             cellTech = GameObject.FindGameObjectWithTag("CellTag").GetComponent<CellTech>();
-            
-            lossButton.onClick.AddListener(() =>
-            {
-                Loader.LoadScene(Loader.Scene.LossDialogue.ToString());
-            })  ;  
+        
 
             winButton.onClick.AddListener(() =>
             {
